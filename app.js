079e25ede34e966308cdfa5ce2af280a43b8b584
Edmundo Pichardo <ce.pichardo@gmail.com>
2023-01-15 05:29:50 -0400

    Se implemento el input con inquirer

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-import { inquirerMenu } from './helpers/inquirer.js';
+import { inquirerMenu, pausa} from './helpers/inquirer.js';
 
 console.clear();
 
 const main = async () => {
 
    let opt = '';
-
+   let input  = '';
    do {
 
      opt = await inquirerMenu();
      console.log({opt});
-
+     input = await pausa();
+     if(input === '0')  break;
+     
    } while (opt !== '0');
 
 }
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -5,10 +5,47 @@ import colors from 'colors';
 const preguntas = [
    {
       type: 'list',
-      name: 'option',
+      name: 'opcion',
       message: 'Que desea hacer?',
-      choices: ['opt1', 'opt2', 'opt3']
+      choices: [
+         {
+            value: '1',
+            name: '1. Crear tarea'
+         },
+         {
+            value: '2',
+            name: '2. Listar tarea'
+         },
+         {
+            value: '3',
+            name: '3. Listar tareas completadas'
+         },
+         {
+            value: '4',
+            name: '4. Listar tareas Pendientes'
+         },
+         {
+            value: '5',
+            name: '5. Completar Tareas'
+         },
+         {
+            value: '6',
+            name: '6. Borrar tarea'
+         },
+         {
+            value: '0',
+            name: '0. Salir'
+         },
+      ]
    }
+];
+
+const entrada = [
+   {
+      type: 'input',
+      name: 'valor',
+      message: `Presione ${'ENTER'.green} para continuar:`,
+    },
 ];
 
 const inquirerMenu = async () => {
@@ -17,8 +54,13 @@ const inquirerMenu = async () => {
    console.log('    Seleccione una opción     '.yellow);
    console.log('==============================\n'.green);
 
-   const opt = await inquirer.prompt(preguntas);
-   return opt;
+   const { opcion } = await inquirer.prompt(preguntas);
+   return opcion;
 }
 
-export { inquirerMenu };
+const pausa = async () => {
+   const { valor } = await inquirer.prompt(entrada);
+   return valor;
+}
+
+export { inquirerMenu, pausa };
